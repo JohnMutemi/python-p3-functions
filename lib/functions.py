68b8bfeb08a748e28eb2da954615b94a981450a9
JohnMutemi <jmtutorsalp@gmail.com>
2024-05-21 22:01:21 +0300

    commit functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
-
+                  
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return num1+ num2
+
+result_add=add(45,55)
+print(result_add)
 
 def halve(number):
-    pass
+    return number/2
+
+result_halve=halve(100)
+print(result_halve)
